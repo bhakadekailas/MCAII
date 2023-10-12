492b665453c30d1843ad55d820abb69c58cee4c8
Kailas <k.bhakade349@gmail.com>
2023-10-12 11:33:20 +0530

    webview demo added.

--- app/webviewdemo/src/main/java/com/kailas/webviewdemo/MainActivity.java
+++ app/webviewdemo/src/main/java/com/kailas/webviewdemo/MainActivity.java
@@ -2,13 +2,45 @@ package com.kailas.webviewdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webView);
+        progressBar = findViewById(R.id.progressBar);
+        webView.loadUrl("https://imcc.mespune.in/");
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
