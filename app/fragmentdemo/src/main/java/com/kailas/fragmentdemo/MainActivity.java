d7a03e138f3d8bec5d6e64a5e578d06dd399039e
Kailas <k.bhakade349@gmail.com>
2024-08-24 20:00:25 +0530

    no changed

--- app/fragmentdemo/src/main/java/com/kailas/fragmentdemo/MainActivity.java
+++ app/fragmentdemo/src/main/java/com/kailas/fragmentdemo/MainActivity.java
@@ -6,7 +6,6 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,21 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 
     public void loadFragmentOne(View view) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.fragment_container, new FragmentOne()).commit();
-
 //        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, new FragmentOne()).commit();
     }
 
     public void loadFragmentTwo(View view) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_container, new FragmentTwo()).commit();
+        fragmentTransaction.add(R.id.fragment_container, new FragmentTwo()).commit();
     }
 }
